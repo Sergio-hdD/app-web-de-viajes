5965363fe3ee7aba1af2bc9de0ae24398df0149c
Sergio-hdD <sergio-hdd@hotmail.com>
2023-07-04 22:50:35 -0300

    Retoques

--- src/main/java/com/system/Sistemadeviajes/util/PassGenerator.java
+++ src/main/java/com/system/Sistemadeviajes/util/PassGenerator.java
@@ -9,10 +9,11 @@
 //	
 //
 //		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(4);
-//        //El String que mandamos al metodo encode es el password que queremos encriptar.
-//	System.out.println(bCryptPasswordEncoder.encode("password"));
-//
-//	
+//        
+//		//El String que mandamos al metodo encode es la password que queremos encriptar.
+//		String passwordAEncriptar = "SuPassword";
+//		
+//		System.out.println("La contraseña " + passwordAEncriptar + " encritada es: \n\n" + bCryptPasswordEncoder.encode(passwordAEncriptar));	
 //
 //	}
 //}
